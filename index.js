f7893404ee5c5a04d2abc9285d3f9df4babf3421
OchreFox <contact@ochrefox.net>
2022-05-11 10:17:32 -0500

    Fixed Blitz new depth property

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ const mergeItems = async (endpoints, latestVersion) => {
     "nicknames",
     "requiredChampion",
     "simpleDescription",
-    "tier",
   ];
   const admittedClasses = [
     "MAGE",
@@ -68,6 +67,10 @@ const mergeItems = async (endpoints, latestVersion) => {
               value2 !== null
             ) {
               data[key][key2] = value2.map(Number);
+            } else if (key2 === "depth") {
+              // Rename key2 from depth to tier
+              data[key]["tier"] = value2;
+              delete data[key]["depth"];
             }
           });
         });
