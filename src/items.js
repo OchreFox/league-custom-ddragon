842194ffd5282de94bb54e863e9e8d3b75f928a2
OchreFox <contact@ochrefox.net>
2022-05-12 16:26:23 -0500

    Fixed tier in items.json

--- src/items.js
+++ src/items.js
@@ -24,6 +24,7 @@ const mergeItems = async (endpoints, latestVersion) => {
     "nicknames",
     "requiredChampion",
     "simpleDescription",
+    "tier",
   ];
   const admittedClasses = [
     "MAGE",
@@ -52,8 +53,7 @@ const mergeItems = async (endpoints, latestVersion) => {
             ) {
               data[key][key2] = value2.map(Number);
             } else if (key2 === "depth") {
-              // Rename key2 from depth to tier
-              data[key]["tier"] = value2;
+              // Delete the depth key
               delete data[key]["depth"];
             }
           });
