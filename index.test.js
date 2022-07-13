8a5c7aad7484ef04ca95b7ad12f8361170d3647e
OchreFox <contact@ochrefox.net>
2022-07-13 15:13:47 -0500

    Fixed Jest test config

--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
-const axios = require("axios");
-const { getLatestVersion } = require("./src/utils/getLatestVersion");
-const { matchersWithOptions } = require("jest-json-schema");
-const schemaItems = require("./endpoints/items.schema.json");
-const items = require("./data/latest/items.json");
+import { readFileSync, existsSync } from "fs";
+import { get } from "axios";
+import { getLatestVersion } from "./src/utils/getLatestVersion";
+import { matchersWithOptions } from "jest-json-schema";
+import schemaItems from "./endpoints/items.schema.json";
+import items from "./data/latest/items.json";
 
 expect.extend(matchersWithOptions());
 
@@ -13,7 +13,7 @@ test("Latest version exists in Blitz API", async () => {
   // Create endpoints array
   let endpoints = [];
   // Read the items.json configuration file
-  const itemsConfig = JSON.parse(fs.readFileSync("./endpoints/items.json"));
+  const itemsConfig = JSON.parse(readFileSync("./endpoints/items.json"));
   // Fetch the items.json from the itemsConfig
   itemsConfig.forEach((endpoint) => {
     const url = `${endpoint.baseUrl}${
@@ -24,8 +24,7 @@ test("Latest version exists in Blitz API", async () => {
   // Get endpoint where endpoint.name === "Blitz"
   const blitzEndpoint = endpoints.find((endpoint) => endpoint.name === "Blitz");
   // Fetch item.json from Blitz API
-  await axios
-    .get(blitzEndpoint.url)
+  await get(blitzEndpoint.url)
     .then((response) => {
       expect(response.status).toBe(200);
       return response.data;
@@ -43,8 +42,8 @@ test("Latest version exists in Blitz API", async () => {
 // Test to expect a creation of a latest version directory and a items.json file in it
 test("Latest items.json file is created in folders", async () => {
   const latestVersion = await getLatestVersion();
-  expect(fs.existsSync(`./data/${latestVersion}/items.json`)).toBe(true);
-  expect(fs.existsSync(`./data/latest/items.json`)).toBe(true);
+  expect(existsSync(`./data/${latestVersion}/items.json`)).toBe(true);
+  expect(existsSync(`./data/latest/items.json`)).toBe(true);
 });
 
 // Test to validate the final schema of the items.json file
@@ -55,6 +54,6 @@ test("Latest items.json file has valid schema", () => {
 // Test to expect a creation of a latest champions.json file in the latest version directory
 test("Latest champions.json file is created in folders", async () => {
   const latestVersion = await getLatestVersion();
-  expect(fs.existsSync(`./data/${latestVersion}/champions.json`)).toBe(true);
-  expect(fs.existsSync(`./data/latest/champions.json`)).toBe(true);
+  expect(existsSync(`./data/${latestVersion}/champions.json`)).toBe(true);
+  expect(existsSync(`./data/latest/champions.json`)).toBe(true);
 });
